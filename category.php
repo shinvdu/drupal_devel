9a1a8f6caeca1a2cf6f03b1b1aab3accac4d9ef0
silas.xie <silas.xie@vidaxl.com>
2013-12-13 10:34:36 +0800

    category, rebuild, toogle into function

--- category.php
+++ category.php
@@ -3,43 +3,52 @@ chdir('/home/silas/app/cms/');//change to drupal root
 require_once './includes/bootstrap.inc';
 drupal_bootstrap(DRUPAL_BOOTSTRAP_FULL);
 
-$sql = 'select td.name, ac.* from {term_data} td inner join {woger_amazon_categories_nid} ac on td.tid = ac.term_id where td.vid=9 order by td.tid, ac.language';
-$result = db_query($sql);
-$recored = array();
-while($item = mysql_fetch_array($result)){
-    if (isset($temp) && $item['term_id'] == $temp['term_id'] && $item['language'] == $temp['language']){
-        if (!isset($recored[$item['term_id'].'-'.$item['language']])) {
-            $recored[$item['term_id'].'-'.$item['language']][] = $temp;
+$records = get_data_and_sort(); // query and sort data
+$output = get_final_clean($records); // build the responding array
+array_to_csv_download($output,'export.csv',','); // output as csv file
+
+function get_data_and_sort(){
+    $sql = 'select td.name, ac.* from {term_data} td inner join {woger_amazon_categories_nid} ac on td.tid = ac.term_id where td.vid=9 order by td.tid, ac.language';
+    $result = db_query($sql);
+    $records = array();
+    while($item = mysql_fetch_array($result)){
+        if (isset($temp) && $item['term_id'] == $temp['term_id'] && $item['language'] == $temp['language']){
+            if (!isset($records[$item['term_id'].'-'.$item['language']])) {
+                $records[$item['term_id'].'-'.$item['language']][] = $temp;
+            }
+            $records[$item['term_id'].'-'.$item['language']][] = $item;
         }
-        $recored[$item['term_id'].'-'.$item['language']][] = $item;
+        $temp = $item;
     }
-    $temp = $item;
+    return $records;
 }
-$output = array();
-$amazon_categories = all_amazon_category();
-foreach ($recored as  $group) {
-    $output[] = array(
-        'term_id' => '-----',
-        'term_name' => '-----',
-        'amazon_categary_id' => '-----',
-        'amazon_categary_name' => '-----',
-        'language' => '-----',
-        'url' => '-----',
-    );
-    foreach ($group as $single) {
+
+function get_final_clean($records){
+    $output = array();
+    $amazon_categories = all_amazon_category();
+    foreach ($records as  $group) {
         $output[] = array(
-            'term_id' => $single['term_id'],
-            'term_name' => $single['name'], 
-            'amazon_categary_id' => $single['woger_amazon_categories_id'],
-            'amazon_categary_name' => $amazon_categories[$single['woger_amazon_categories_id']]['name'],
-            'language' => $single['language'],
-            'url' => 'woger_amazon?term='.$single['term_id']. '&lang=' . $single['language'],
+            'term_id' => '-----',
+            'term_name' => '-----',
+            'amazon_categary_id' => '-----',
+            'amazon_categary_name' => '-----',
+            'language' => '-----',
+            'url' => '-----',
         );
+        foreach ($group as $single) {
+            $output[] = array(
+                'term_id' => $single['term_id'],
+                'term_name' => $single['name'], 
+                'amazon_categary_id' => $single['woger_amazon_categories_id'],
+                'amazon_categary_name' => $amazon_categories[$single['woger_amazon_categories_id']]['name'],
+                'language' => $single['language'],
+                'url' => 'woger_amazon?term='.$single['term_id']. '&lang=' . $single['language'],
+            );
 
+        }
     }
+    return $output;
 }
-
-array_to_csv_download($output,'export.csv',',');
 
 function array_to_csv_download($array, $filename = "export.csv", $delimiter=";") {
     $f = fopen('php://memory', 'w'); 
